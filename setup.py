f2d1b2586781e97ba02500df7d76acbc9b53e5f1
Hylobate <Hylobate@users.noreply.github.com>
2023-08-16 11:15:49 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for unmanned_aircraft_project package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("unmanned_aircraft_project", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="unmanned_aircraft_project",
+    version=read("unmanned_aircraft_project", "VERSION"),
+    description="Awesome unmanned_aircraft_project created by Hylobate",
+    url="https://github.com/Hylobate/unmanned-aircraft-project/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="Hylobate",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["unmanned_aircraft_project = unmanned_aircraft_project.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
